96013f56c82aeb2ab871c477449bf387aef35644
MuhammadMathori <mathorimuhammad449@gmail.com>
2023-10-26 10:19:22 +0700

    menambahkan fungsi pesan

--- dist/js/script.js
+++ dist/js/script.js
@@ -52,3 +52,23 @@ if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.match
 } else {
   darkToggle.checked = false;
 }
+
+//function from
+const contactForm = document.getElementById("Contact-Form");
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      //url Teriam Kasih
+      window.location.href = "thankyou.html";
+    })
+    .catch((e) => alert("Error"));
+});
